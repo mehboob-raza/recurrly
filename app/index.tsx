7f0d84cccceb68b4ddd9881a537fa96971962624
unknown <razamehboob61@gmail.com>
2026-05-19 23:25:23 +0500

    addeD

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind its a csss uwhwuwh 
+        Welcome to Nativewind 
       </Text>
     </View>
   );
